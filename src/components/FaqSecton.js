9a247679f19e39c8e0a535c33b87846c4b97520e
Sumerian812 <veronica_hannibal@outlook.com>
2021-01-31 17:26:20 +0100

    FAQ Section

---
+++ src/components/FaqSecton.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+const FaqSection = () => {
+    return (
+        <div className="faq">
+            <h2>Any questions <span>FAQ</span></h2>
+            <div className="question">
+                <h4>How do I start?</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, consectetur.</p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>What products do you offer?</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, consectetur.</p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>Daily schedule</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, consectetur.</p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>Payment methods</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, consectetur.</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default FaqSection;
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,12 +1,14 @@
 import React from "react";
 import AboutSection from "../components/AboutSection";
-import ServicesSection from "../components/ServiceSection"
+import ServicesSection from "../components/ServiceSection";
+import FaqSection from "../components/FaqSecton";
 
 const AboutUs = () => {
     return (
         <>
             <AboutSection />
             <ServicesSection />
+            <FaqSection />
         </>
     );
 }
