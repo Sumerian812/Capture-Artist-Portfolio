1eb95bdf256875a357a10de2b01c0cf86d97f4c4
Sumerian812 <veronica_hannibal@outlook.com>
2021-02-13 16:57:09 +0100

    FAQ styling

--- src/components/FaqSecton.js
+++ src/components/FaqSecton.js
@@ -1,39 +1,72 @@
 import React from "react";
+import styled from "styled-components";
+import { StyledAbout } from "../styles";
 
 const FaqSection = () => {
     return (
-        <div className="faq">
-            <h2>Any questions <span>FAQ</span></h2>
+        <StyledFaq>
+            <h2>Any questions?<span>FAQ</span></h2>
             <div className="question">
                 <h4>How do I start?</h4>
                 <div className="answer">
                     <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, consectetur.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nulla, qui fugit ea odio aut!</p>
                 </div>
+                <div className="faq-line"></div>
             </div>
             <div className="question">
                 <h4>What products do you offer?</h4>
                 <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur.</p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, consectetur.</p>
                 </div>
+                <div className="faq-line"></div>
             </div>
             <div className="question">
                 <h4>Daily schedule</h4>
                 <div className="answer">
                     <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, consectetur.</p>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et.</p>
                 </div>
+                <div className="faq-line"></div>
             </div>
             <div className="question">
                 <h4>Payment methods</h4>
                 <div className="answer">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
                     <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, consectetur.</p>
                 </div>
+                <div className="faq-line"></div>
             </div>
-        </div>
+        </StyledFaq>
     );
 }
+
+const StyledFaq = styled(StyledAbout)`
+    display: block;
+    span {
+        display: block;
+    }
+    h2 {
+        padding-bottom: 2rem;
+        font-weight: lighter;
+    }
+    .faq-line {
+        background: #cccccc;
+        height: 0.2rem;
+        margin: 2rem 0rem;
+        width: 100%;
+    }
+    .question {
+        padding: 3rem 0rem;
+        cursor: pointer;
+    }
+    .answer {
+        padding: 2rem 0rem;
+        p {
+            padding: 1rem 0rem;
+        }
+    }
+`;
 
 export default FaqSection;
--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -2,25 +2,24 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
 
-*{
+* {
     margin: 0;
     padding: 0;
     box-sizing: border-box
 }
 
-html{
+html {
     @media (max-width: 1700px){
         font-size: 75%;
     }
     
 }
-
-body{
+body {
     background: #1b1b1b;
     font-family: 'Inter', sans-serif;
     overflow-x: hidden;
 }
-button{
+button {
     font-weight: bold;
     font-size: 1.1.rem;
     cursor: pointer;
@@ -35,32 +34,31 @@ button{
         color: white;
     }
 }
-    h2{
-        font-weight: lighter;
-        font-size: 4rem;
-    }
-    h3{
-        color: white;
-    }
-    h4{
-        font-weight: bold;
-        font-size: 2rem;
-       
-    }
-    a{
-        font-size: 1.1rem;
-    }
-    span{
-        font-weight: bold;
-        color:#23d997;
-    }
-    p{
-        padding: 3rem 0rem;
-        color: #ccc;
-        font-size: 1.4rem;
-        line-height: 150%;
-    }
-
+h2 {
+    font-weight: lighter;
+    font-size: 4rem;
+}
+h3 {
+    color: white;
+}
+h4 {
+    font-weight: bold;
+    font-size: 2rem;
+    
+}
+a {
+    font-size: 1.1rem;
+}
+span {
+    font-weight: bold;
+    color:#23d997;
+}
+p {
+    padding: 3rem 0rem;
+    color: #ccc;
+    font-size: 1.4rem;
+    line-height: 150%;
+}
 `;
 
 export default GlobalStyle;
