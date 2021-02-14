d7feac1dfbf00d4e15b8b6d9ff8c299e6095929b
Sumerian812 <veronica_hannibal@outlook.com>
2021-02-14 14:12:13 +0100

    added Nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import AboutUs from "./pages/AboutUs";
 import GlobalStyle from "./components/GlobalStyles";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
+      <Nav />
       <AboutUs />
     </div>
   );
