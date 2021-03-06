fd3a3f1d71032f8a3f5814d756775497bc44173b
Sumerian812 <veronica_hannibal@outlook.com>
2021-03-06 14:49:44 +0100

    AboutSection Animation

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -3,23 +3,14 @@ import home1 from "../img/home1.png"
 import { StyledAbout, StyledImage, StyledHide, StyledDescription } from "../styles";
 // Framer Motion
 import { motion } from "framer-motion";
+import { titleAnimation, fade, photoAnimation } from "../animation";
 
 
 const AboutSection = () => {
-    const titleAnimation = {
-        hidden: { opacity: 0 },
-        show: { opacity: 1, transition: { duration: 1 } }
-    };
-
-    const container = {
-        hidden: { x: 100 },
-        show: { x: 0, transition: { ease: "easeOut", duration: 0.75, staggerChildren: 0.75 } }
-    }
-
     return (
         <StyledAbout>
             <StyledDescription>
-                <motion.div variants={container} initial="hidden" animate="show" className="title">
+                <motion.div className="title">
                     <StyledHide>
                         <motion.h2 variants={titleAnimation}>
                             We work to make
@@ -32,14 +23,14 @@ const AboutSection = () => {
                         <motion.h2 variants={titleAnimation}> come true.</motion.h2>
                     </StyledHide>
                 </motion.div>
-                <p>
+                <motion.p variants={fade}>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                     Donec hendrerit sodales augue, ac aliquam ligula varius vel.
-                </p>
-                <button>Contact us</button>
+                </motion.p>
+                <motion.button variants={fade}>Contact us</motion.button>
             </StyledDescription>
             <StyledImage>
-                <img src={home1} alt="" />
+                <motion.img variants={photoAnimation} src={home1} alt="" />
             </StyledImage>
         </StyledAbout>
     )
